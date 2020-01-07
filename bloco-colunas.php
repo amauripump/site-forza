b79cdb4d35a657b9a87ad34a1e9ec26c20b10573
Amauri da Silva Junior <amauri@gluck.com.br>
2020-01-07 11:14:53 -0300

    Commit das primeiras alterações no Front, criação da home

---
+++ bloco-colunas.php
@@ -0,0 +1,63 @@
+  <!--==========================
+    Para empresas
+  ============================-->
+  <section id="para-empresas">
+    <div class="container p-5">
+      <div class="row">
+
+        <div class="col-md-6 col-sm-12">
+          <h2 class="verde">Forza</h2>
+          <h2>Para a sua empresa</h2>
+
+          <div class="column-item">
+            <h3 class="verde">Treinamentos e Palestras</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+          <div class="column-item">
+            <h3 class="verde">Estruturação de RH</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+          <div class="column-item">
+            <h3 class="verde">Avaliação Psicológica e de Potencial</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+          <div class="column-item">
+            <h3 class="verde">Desenvolvimento de Lideranças</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+        </div>
+
+        <div class="col-md-6">
+          <h2 class="roxo">Forza</h2>
+          <h2>Para você mulher</h2>
+          
+          <div class="column-item">
+            <h3 class="roxo">Empreendedorismo Feminino</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+          <div class="column-item">
+            <h3 class="roxo">Reposicionamento Profissional</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+          <div class="column-item">
+            <h3 class="roxo">Desenvolvimento de Lideranças</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+          <div class="column-item">
+            <h3 class="roxo">Maternidade e Carreira</h3>
+            <p>Realizamos cursos e treinamentos personalizados conforme a necessidade da sua empresa.</p>
+          </div>
+
+        </div>
+  
+      </div>
+
+    </div>
+  </section><!-- #para empresas -->
---
+++ footer.php
@@ -0,0 +1,77 @@
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer" class="section-bg">
+    <div class="footer-top">
+      <div class="container">
+
+            <div class="row">
+
+                <div class="col-sm-3">
+
+                  <div class="footer-info">
+                  <a href="#header" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a>
+                  </div>
+
+                </div>
+                <div class="col-sm-3">
+
+                  <div class="footer-info">
+                    (62) 98888-9090 | 3456-7890
+                  </div>
+
+                </div>
+                <div class="col-sm-3">
+
+                  <div class="footer-info">
+                    vagas@forza
+                  </div>
+
+                </div>
+                <div class="col-sm-3">
+
+                  <div class="footer-info">
+                    Rua 70
+                  </div>
+
+                </div>
+
+
+            </div>
+
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; 2019 - Todos direitos reservados
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/mobile-nav/mobile-nav.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
